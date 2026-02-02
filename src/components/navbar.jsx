15ac2ab5ed5d07d66851626faad1b47a43b7d049
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2026-02-02 10:45:10 +0700

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 import { navlinks } from "../Data/navbarmenu";
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    
+
     return (
         <>
             <motion.nav className="sticky top-0 z-50 flex items-center justify-between w-full h-18 px-6 md:px-16 lg:px-24 xl:px-32 backdrop-blur"
@@ -28,7 +28,7 @@ export default function Navbar() {
 
                 <div className="hidden lg:block space-x-3">
                     <button className="hover:bg-slate-300/20 transition px-6 py-2 border border-slate-400 rounded-md active:scale-95">
-                       <Link to="/login">เข้าสู่ระบบ</Link> 
+                        <Link to="/login">เข้าสู่ระบบ</Link>
                     </button>
                     <button className="hover:bg-slate-300/20 transition px-6 py-2 border border-slate-400 rounded-md active:scale-95">
                         <Link to="/register">สมัครสมาชิก</Link>
@@ -44,12 +44,18 @@ export default function Navbar() {
                         {link.text}
                     </Link>
                 ))}
+                <button className="hover:bg-slate-300/20 transition px-6 py-2 active:scale-95">
+                    <Link to="/login">เข้าสู่ระบบ</Link>
+                </button>
+                <button className="hover:bg-slate-300/20 transition px-6 py-2 active:scale-95">
+                    <Link to="/register">สมัครสมาชิก</Link>
+                </button>
                 <button onClick={() => setIsMenuOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-slate-100 hover:bg-slate-200 transition text-black rounded-md flex">
                     <XIcon />
                 </button>
             </div>
             <div>
-                <Outlet/>
+                <Outlet />
             </div>
         </>
     );
